994f3a3e271b1790663da56325befcae4c037573
Shivnath <35649358+shivnath17@users.noreply.github.com>
2024-03-01 05:53:57 +0000

    updated latest selectors

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 const SPACE = "Space"
-const INITIAL_CAMERA_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.GOH7Zb > div > div"
-const INITIAL_MIC_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.ZB88ed > div > div > div"
-const JOIN_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div > div.d7iDfe.NONs6c > div > div.Sla0Yd > div > div.XCoPyb > div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt"
-const MIC_SELECTOR = "#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.rG0ybd.xPh1xb.P9KVBf > div.Kn8SEb > div > div.a1GRr.PjGUeb > div > div > span > button > div.VfPpkd-Bz112c-Jh9lGc"
+const INITIAL_CAMERA_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(25) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.XDitY.NONs6c > div > div.EhAUAc > div.GOH7Zb > div > div.U26fgb.JRY2Pb.mUbCce.kpROve.yBiuPb.y1zVCf.HNeRed.M9Bg4d"
+const INITIAL_MIC_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(25) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.ZB88ed > div > div > div.U26fgb.JRY2Pb.mUbCce.kpROve.yBiuPb.y1zVCf.HNeRed.M9Bg4d"
+const JOIN_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(25) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb > div:nth-child(1) > button > div.VfPpkd-RLmnJb"
+const MIC_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(24) > div.crqnQb > div.fJsklc.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div > div.fswXR > div > div.GKGgdd > span > button"
 
 
 const sleep = async (msec) => {
