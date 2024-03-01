964d271d6cbe4910b3ccee104b28b8b2f9e3fa14
Shivnath <35649358+shivnath17@users.noreply.github.com>
2024-03-01 07:09:25 +0000

    migrated to menifist v3

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 const SPACE = "Space"
-const INITIAL_CAMERA_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(25) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.XDitY.NONs6c > div > div.EhAUAc > div.GOH7Zb > div > div.U26fgb.JRY2Pb.mUbCce.kpROve.yBiuPb.y1zVCf.HNeRed.M9Bg4d"
-const INITIAL_MIC_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(25) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.ZB88ed > div > div > div.U26fgb.JRY2Pb.mUbCce.kpROve.yBiuPb.y1zVCf.HNeRed.M9Bg4d"
-const JOIN_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(25) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb > div:nth-child(1) > button > div.VfPpkd-RLmnJb"
+const INITIAL_CAMERA_TOOLTIP = "Turn off camera (ctrl + e)"
+const INITIAL_MIC_TOOLTIP = "Turn off microphone (ctrl + d)"
+const JOIN_SELECTOR = '//*[@id="yDmH0d"]/c-wiz/div/div/div[24]/div[3]/div/div[2]/div[4]/div/div/div[2]/div[1]/div[2]/div[1]/div[1]/button'
+
 const MIC_SELECTOR = "#yDmH0d > c-wiz > div > div > div:nth-child(24) > div.crqnQb > div.fJsklc.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div > div.fswXR > div > div.GKGgdd > span > button"
 
 
@@ -9,16 +10,22 @@ const sleep = async (msec) => {
     return new Promise(resolve => setTimeout(resolve, msec));
 }
 
+const getElementByXpath = (path) => {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
 
 window.addEventListener('load', async () => {
-    const INITIAL_CAMERA = document.querySelector(INITIAL_CAMERA_SELECTOR)
-    const INITIAL_MIC = document.querySelector(INITIAL_MIC_SELECTOR)
-    const JOIN = document.querySelector(JOIN_SELECTOR)
+    await sleep(3000);
+
+    const INITIAL_CAMERA = document.querySelector(`[data-tooltip='${INITIAL_CAMERA_TOOLTIP}']`)
+    const INITIAL_MIC = document.querySelector(`[data-tooltip='${INITIAL_MIC_TOOLTIP}']`)
+    const JOIN = getElementByXpath(JOIN_SELECTOR)
 
     INITIAL_CAMERA.click()
     INITIAL_MIC.click()
     await sleep(5000);
-    JOIN.click()
+    JOIN?.click()
 
     let micStatus = false
 
